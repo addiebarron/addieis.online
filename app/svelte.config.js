0440030fe359a5d557baaa265c955c561480d6a3
addiebarron.os <addie.barron@outschool.com>
2022-06-21 14:55:46 -0400

    Switch to adapter netlify

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,10 @@
-import adapterStatic from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-netlify";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapterStatic(),
+    adapter: adapter(),
   },
   preprocess: preprocess(),
 };
