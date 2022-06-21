9fe920971d1932b852da49507431f02a745425a6
addiebarron.os <addie.barron@outschool.com>
2022-06-21 14:48:37 -0400

    Switch to adapter static

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,10 @@
-import node from "@sveltejs/adapter-node";
+import adapterStatic from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: node(),
+    adapter: adapterStatic(),
   },
   preprocess: preprocess(),
 };
